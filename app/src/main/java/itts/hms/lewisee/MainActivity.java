8346f316f87f19f8da2843f72a8c0e7d09c8feac
hilewi <lewilawisa@gmail.com>
2022-04-15 00:41:30 +0700

    initial commit

---
+++ app/src/main/java/itts/hms/lewisee/MainActivity.java
@@ -0,0 +1,14 @@
+package itts.hms.lewisee;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
